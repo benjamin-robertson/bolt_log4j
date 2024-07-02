264352fbc437ca966b6045a8893390ac7587ffe2
ben <benrobertson9876@gmail.com>
2024-07-02 16:30:37 +1000

    push

--- plans/vuln.pp
+++ plans/vuln.pp
@@ -11,14 +11,30 @@ plan bolt_log4j::vuln (
 ) {
   $final_target = get_targets($targets)
 
+  # collect facts
+  run_plan('facts', 'targets' => $targets)
+  $linux_targets = get_targets($targets).filter | $n | { $n.factsp['kernel'] == 'Linux' }
+  $win_targets = get_targets($targets).filter | $n | { $n.factsp['kernel'] == 'Windows' }
+
   # Read file
   $win_scanner = file::read('bolt_log4j/log4jscanner-v0.5.0-windows-amd64.zip')
   $nix_scanner = file::read('bolt_log4j/log4jscanner-v0.5.0-linux-amd64.tar.gz')
   out::message("targets are ${targets}")
 
   # copy scanner
-  $file_results = write_file($nix_scanner, '/tmp', $final_target, { '_run_as' => 'root' })
+  if $win_targets.length >= 1 {
+    $win_file_results = write_file($win_scanner, '/tmp/log4jscanner-v0.5.0-windows-amd64.zip', $win_targets, { '_run_as' => 'root', '_catch_errors' => true })
+    $win_file_successful = $win_file_results.ok_set
+    $win_file_failed = $win_file_results.error_set.names
+    $win_file_eligible_targets = $win_targets - get_targets($win_file_failed)
+  }
+  if $linux_targets.length >= 1 {
+    $linux_file_results = write_file($linux_targets, '/tmp/log4jscanner-v0.5.0-linux-amd64.tar.gz', $linux_targets, { '_run_as' => 'root', '_catch_errors' => true })
+    $linux_file_successful = $linux_file_results.ok_set
+    $linux_file_failed = $linux_file_results.error_set.names
+    $linux_file_eligible_targets = $linux_targets - get_targets($linux_file_failed)
+  }
 
   # $command_result = run_command('whoami', $targets)
-  return $file_results
+  return $linux_file_results
 }
