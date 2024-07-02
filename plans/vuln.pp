0c637abf8c35336f578bb17edf8578ff737c5297
ben <benrobertson9876@gmail.com>
2024-07-02 17:54:32 +1000

    push with deps

--- plans/vuln.pp
+++ plans/vuln.pp
@@ -7,7 +7,9 @@
 # @summary A plan created with bolt plan new.
 # @param targets The targets to run on.
 plan bolt_log4j::vuln (
-  TargetSpec $targets,
+  TargetSpec         $targets,
+  Stdlib::Windowpath $windows_file_path = 'c:\\',
+  Stdlib::Unixpath   $linux_file_path   = '/tmp'
 ) {
   $final_target = get_targets($targets)
 
@@ -23,18 +25,19 @@ plan bolt_log4j::vuln (
 
   # copy scanner
   if $win_targets.length >= 1 {
-    $win_file_results = write_file($win_scanner, '/tmp/log4jscanner-v0.5.0-windows-amd64.zip', $win_targets, { '_run_as' => 'root', '_catch_errors' => true })
+    $win_file_results = write_file($win_scanner, "${windows_file_path}/log4jscanner-v0.5.0-windows-amd64.zip", $win_targets, { '_run_as' => 'root', '_catch_errors' => true })
     $win_file_successful = $win_file_results.ok_set
     $win_file_failed = $win_file_results.error_set.names
     $win_file_eligible_targets = $win_targets - get_targets($win_file_failed)
   }
   if $linux_targets.length >= 1 {
-    $linux_file_results = write_file($nix_scanner, '/tmp/log4jscanner-v0.5.0-linux-amd64.tar.gz', $linux_targets, { '_run_as' => 'root', '_catch_errors' => true })
+    $linux_file_results = write_file($nix_scanner, "${linux_file_path}/log4jscanner-v0.5.0-linux-amd64.tar.gz", $linux_targets, { '_run_as' => 'root', '_catch_errors' => true })
     $linux_file_successful = $linux_file_results.ok_set
     $linux_file_failed = $linux_file_results.error_set.names
     $linux_file_eligible_targets = $linux_targets - get_targets($linux_file_failed)
   }
 
-  # $command_result = run_command('whoami', $targets)
+  #Apply block, extract the 
+
   return $linux_file_results
 }
