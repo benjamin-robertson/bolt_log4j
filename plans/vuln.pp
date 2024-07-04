0ab3b34d9133a3ca208401ddf790f8b687cf563d
ben <benrobertson9876@gmail.com>
2024-07-04 12:23:21 +1000

    create hash

--- plans/vuln.pp
+++ plans/vuln.pp
@@ -67,11 +67,34 @@ plan bolt_log4j::vuln (
 
   # out::message("Apply results: ${linux_apply_results}")
 
-  $linux_vuln_results = run_command('/tmp/log4jscanner/log4jscanner /', $linux_apply_okay_targets, '_catch_errors' => true, '_run_as' => 'root')
+  $linux_vuln_results = run_command('/tmp/log4jscanner/log4jscanner /', $linux_apply_okay_targets, '_catch_errors' => true, '_run_as' => 'root' )
 
   out::message("vuln results ${linux_vuln_results}")
 
   # Get vulnerable systems
   $vulnerable_systems = $linux_vuln_results.ok_set.filter | $result | { $result.value['stdout'].length > 1 }
+
+  # Get failed systems
+  $errored_systems = $linux_vuln_results.error_set.names
+
+  $original_errored_systems = defined('$errored_systems') ? {
+    true    => $errored_systems,
+    default => {},
+  }
+
+  $original_vulnerable_systems = defined('$vulnerable_systems') ? {
+    true    => $vulnerable_systems,
+    default => {},
+  }
+
+  $vulnerable_results = $original_vulnerable_systems.reduce | $memo, $value | {
+    $memo + { $value.target => $value.value['stdout'] }
+  }
+
+  $summary_results = {
+    'errored_systems'    => $original_errored_systems,
+    'vulnerable_systems' => $vulnerable_results,
+  }
+
   return $vulnerable_systems
 }
