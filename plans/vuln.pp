932bfc8810f224ff378c4afd7647f999b51cdca9
ben <benrobertson9876@gmail.com>
2024-07-04 13:49:44 +1000

    only stdout

--- plans/vuln.pp
+++ plans/vuln.pp
@@ -88,7 +88,7 @@ plan bolt_log4j::vuln (
   }
 
   $vulnerable_results = $original_vulnerable_systems.reduce | $memo, $value | {
-    out::message("${value.target} ${value.value}")
+    out::message("${value.target} ${value.value['stdout']}")
     $memo + { $value.target => $value.value['stdout'] }
   }
 
